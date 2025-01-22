05bf488483b7d5dfbec34e5a3da8f1c4ac53a0c9
221B308 <221B308@juet.co.in>
2025-01-22 14:24:13 +0530

    lE-1

---
+++ LE1/QUESTION 1(221b308)/Main.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+class Mother {
+    int x;
+    Mother() {
+        x = 10;  
+    }
+    void show() {
+        System.out.println("Mother's x: " + x);
+    }
+}
+class Child extends Mother {
+    
+}
+class Main {
+    public static void main(String args[]) {
+        Mother m = new Mother();
+        m.show(); 
+        Child ch = new Child();
+        ch.show(); 
+    }
+}
